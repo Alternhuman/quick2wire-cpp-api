1579dde037983fe48cf9ff8a199a00eb4897d7a5
Alarmpi <alarmpi@martinarroyo.net>
2015-06-01 14:33:19 +0000

    Fixed problem with revision

--- src/board_revision.cpp
+++ src/board_revision.cpp
@@ -7,8 +7,9 @@ int revision(){
 	std::string str;
 	std::string s("Revision");
 	while(std::getline(f, str)){
-		printf("%s\n", str.c_str());
-		if(s.compare(0, s.length(), str) == 0){
+		//printf("%s %d\n", str.c_str(), s.compare(0, s.length(), str.substr(0, s.length())));
+		if(s.compare(0, s.length(), str.substr(0, s.length())) == 0){
+			//printf("%s\n", str.c_str());
 			if((str.compare(str.length() - 2, str.length() -1, "2") == 0) || (str.compare(str.length() - 2, str.length() - 1, "3") == 0)){
 				return 1;
 			}
