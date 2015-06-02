2c636ba04ced8a0169a0050a9320b589850a7f07
Alarmpi <alarmpi@martinarroyo.net>
2015-06-02 10:39:54 +0000

    Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,9 @@
 #include "board_revision.hpp"
 #include "gpio.hpp"
 #include <unistd.h>
-/*from quick2wire.gpio import pins, In, Out
-
-#pins_number = [22, 4, 17, 27, 18, 23, 24, 25]
-pins_number = [3, 7, 0, 2, 1, 4, 5]
-pins_obj=[]
-for pin in pins_number:
-	pins_obj.append(pins.pin(pin, direction=Out))
-def sigint_handler(signal, frame):
-	for pin in pins_obj:
-		pin.close()
-	sys.exit(0)
-signal.signal(signal.SIGINT, sigint_handler)
-while True:
-	for i in range(0, 3):
-		for pin in pins_obj:
-			pin.open()
-			pin.value = 1
-			time.sleep(SLEEP_TIME)
-			pin.value=0
-			pin.close()*/
-
 #include <vector>
+#include <time.h>
+#include <math.h>
 
 #define PIN_COUNT 7
 using namespace std;
@@ -31,8 +12,17 @@ int main(int argc, char* argv[]){
 	GPIO g;
 	int pin_numbers [PIN_COUNT] = {3, 7, 0, 2, 1, 4, 5};
 	vector <Pin> pins;
-
-	int sleep_time = atoi(argv[1]);
+	if(argc < 2){
+		fprintf(stdout, "Usage %s [sleep time in seconds]", argv[0]);
+		exit(1);
+	}
+	
+	double sleep_time = atof(argv[1]);
+	if(sleep_time < 0){
+		fprintf(stdout, "Wrong sleep time value");
+		exit(2);
+	}
+//	fprintf(stdout, "%lf", sleep_time);
 
 	for (int i = 0; i < PIN_COUNT; ++i)
 	{
@@ -48,7 +38,7 @@ int main(int argc, char* argv[]){
 			
 			pins[i].setDirection(OUT);
 			pins[i].setValue(1);
-			sleep(sleep_time);
+			usleep(sleep_time*1000000);
 			pins[i].setValue(0);
 		}
 	}
